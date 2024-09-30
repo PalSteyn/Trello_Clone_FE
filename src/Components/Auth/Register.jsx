0c13fec2909902523851973b0a64f8ebb9d8196b
Akshay Bansal <akshay.19b101094@abes.ac.in>
2024-09-30 17:58:24 +0530

    fix loader for signup

--- src/Components/Auth/Register.jsx
+++ src/Components/Auth/Register.jsx
@@ -3,6 +3,7 @@ import styles from "./Login.module.css"; // Importing CSS Module
 import { useNavigate } from "react-router-dom";
 import { signup } from "../../services/authService";
 import { getCookie } from "../../utils/helper";
+import Loader from "../Common/Loader/Loader";
 
 const Register = () => {
   // State for form fields
@@ -12,6 +13,7 @@ const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [loader, setLoader] = useState(false);
 
   const token = getCookie("token");
 
@@ -62,6 +64,7 @@ const Register = () => {
     }
 
     try {
+      setLoader(true);
       const name = firstName + " " + lastName;
       const response = await signup(name, email, password);
       console.log("singup res", response);
@@ -70,66 +73,71 @@ const Register = () => {
     } catch (error) {
       console.error("Login failed", error);
       alert("Sign Up Failed");
+    } finally {
+      setLoader(false);
     }
   };
 
   return (
-    <div className={styles.loginContainer}>
-      <h2 className={styles.title}>Signup</h2>
-      <div className={styles.loginBox}>
-        <form className={styles.loginForm} onSubmit={handleSignup}>
-          <input
-            type="text"
-            placeholder="First Name"
-            className={styles.inputField}
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)} // Update state on change
-            required
-          />
-          <input
-            type="text"
-            placeholder="Last Name"
-            className={styles.inputField}
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            required
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            className={styles.inputField}
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            className={styles.inputField}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            className={styles.inputField}
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-          <button type="submit" className={styles.loginButton}>
-            Signup
-          </button>
-        </form>
-        <p className={styles.signupText}>
-          Already have an account?{" "}
-          <a className={styles.signupLink} onClick={() => navigate("/")}>
-            Login
-          </a>
-        </p>
+    <>
+      <div className={styles.loginContainer}>
+        <h2 className={styles.title}>Signup</h2>
+        <div className={styles.loginBox}>
+          <form className={styles.loginForm} onSubmit={handleSignup}>
+            <input
+              type="text"
+              placeholder="First Name"
+              className={styles.inputField}
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)} // Update state on change
+              required
+            />
+            <input
+              type="text"
+              placeholder="Last Name"
+              className={styles.inputField}
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              required
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              className={styles.inputField}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className={styles.inputField}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              className={styles.inputField}
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+            <button type="submit" className={styles.loginButton}>
+              Signup
+            </button>
+          </form>
+          <p className={styles.signupText}>
+            Already have an account?{" "}
+            <a className={styles.signupLink} onClick={() => navigate("/")}>
+              Login
+            </a>
+          </p>
+        </div>
       </div>
-    </div>
+      {loader && <Loader />}
+    </>
   );
 };
 
